0d26242e2b8f2924ea6ee244178d7ce715dcf5a4
Andrew Lai <the.andrew.lai@gmail.com>
2023-01-06 11:20:24 -0800

    organized code

--- main.py
+++ main.py
@@ -20,36 +20,6 @@ with open("vowels.json","r", encoding='utf8') as json_file:
 with open("voice.json","r", encoding='utf8') as json_file:
     voice = json.load(json_file)
     json_file.close()
-
-# MAIN
-
-def generateCognate(word_lst):
-        word_new = []
-
-        t_pre = False
-        for i in range(len(word_lst)):
-                if word_lst[i] == "p":
-                    word_new.append("f")
-                elif word_lst[i] == "t":
-                    word_new.append("s")
-                    t_pre = True
-                elif word_lst[i] == "b":
-                    word_new.append("p")
-                elif word_lst[i] == "d":
-                    word_new.append("t")
-                elif word_lst[i] == "h" and t_pre == True:
-                    word_new.append("d")
-                    word_new[i-1] = "_"
-                elif word_lst[i] == "w":
-                    word_new.append("v")
-                else:
-                    word_new.append(word_lst[i])
-                
-                if word_lst[i] != "h" and word_lst[i] != "t" and t_pre == True:
-                    t_pre = False
-        
-        word_result = ("".join(word_new).replace("_",""))
-        return word_result
         
 
 """
@@ -105,40 +75,78 @@ elif opt == 2:
         print("".join(word_new).replace("_",""))
 """
 
-# TK
+# MAIN
 
-window = tk.Tk()
-window.geometry('400x300')
-w_title = window.title("IPA-String-info")
-w_header = tk.Label(text="German Cognate").pack()
+def generateCognate(word_lst):
+        word_new = []
 
-# cognate label
-test = tk.Entry(window)
-test.pack(pady=30)
-
-# cognate label init
-label_reg = tk.Label(window, text=f"Regular: empty", pady=20)
-label_cog = tk.Label(window, text=f"Cognate: empty", pady=5)
+        t_pre = False
+        for i in range(len(word_lst)):
+                if word_lst[i] == "p":
+                    word_new.append("f")
+                elif word_lst[i] == "t":
+                    word_new.append("s")
+                    t_pre = True
+                elif word_lst[i] == "b":
+                    word_new.append("p")
+                elif word_lst[i] == "d":
+                    word_new.append("t")
+                elif word_lst[i] == "h" and t_pre == True:
+                    word_new.append("d")
+                    word_new[i-1] = "_"
+                elif word_lst[i] == "w":
+                    word_new.append("v")
+                else:
+                    word_new.append(word_lst[i])
+                
+                if word_lst[i] != "h" and word_lst[i] != "t" and t_pre == True:
+                    t_pre = False
+        
+        word_result = ("".join(word_new).replace("_",""))
+        return word_result
 
 def printCognate():
-    #var = test.get("1.0",tk.END)
-    var = test.get()
+    var = word_entry.get("1.0",tk.END)
+    #var = word_entry.get()
     var_cognate = generateCognate(list(var))
 
     label_reg.config(text=f"Regular: {var}")
     label_cog.config(text=f"Cognate: {var_cognate}")
 
+def to_dm():
+    window.config(bg='black')
+
+# TK
+
+window = tk.Tk()
+window.geometry('400x300')
+w_title = window.title("IPA-String-info")
+w_header = tk.Label(text="Convert to German Cognate", font=("Arial", 15, 'bold'))
+
+# cognate label
+word_entry = tk.Text(window,width=33,height=2)
+
+# cognate label init
+label_reg = tk.Label(window, text=f"Regular: ")
+label_cog = tk.Label(window, text=f"Cognate: ")
+
 # placing widgets 
-tk.Button(
+word_button = tk.Button(
     window,
     text="Generate", 
     padx=10, 
     pady=5,
     command=printCognate
-    ).pack()
+    )
 
-label_reg.pack()
-label_cog.pack()
+dm_button = tk.Button(window, activebackground='black', command=to_dm)
+
+w_header.grid(row=0,column=0)
+word_entry.grid(row=1,column=0)
+word_button.grid(row=1,column=1)
+label_reg.grid(row=3,column=0)
+label_cog.grid(row=4,column=0)
+dm_button.grid(row=5,column=0)
 
 
 window.mainloop()
